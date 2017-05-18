ab7457847b90c16a327ebc99b6c6660707f8b7f3
ramaqst <qisthi.rm@gmail.com>
2017-05-18 19:59:25 +0700

    konfigurasi dan instalasi role laratrust

--- config/app.php
+++ config/app.php
@@ -179,6 +179,7 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         Collective\Html\HtmlServiceProvider::class,
+        Laratrust\LaratrustServiceProvider::class,
 
     ],
 
@@ -229,6 +230,7 @@ return [
 
         'Form'=>Collective\Html\FormFacade::class,
         'Html'=>Collective\Html\HtmlFacade::class,
+        'Laratrust'=>Laratrust\LaratrustFacade::class,
     ],
 
 ];
