ee2c8b6711545d8445c15bacaaa6302a28a8b963
ramaqst <qisthi.rm@gmail.com>
2017-05-18 20:25:28 +0700

    buat role dan member

--- database/seeds/UsersSeeder.php
+++ database/seeds/UsersSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Role;
+use App\User;
 
 class UsersSeeder extends Seeder
 {
@@ -11,6 +13,32 @@ class UsersSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // Membuat role admin
+        $adminRole = new Role();
+        $adminRole->name = 'admin';
+        $adminRole->display_name = 'Admin';
+        $adminRole->save();
+
+        // Membuat role member
+        $memberRole = new Role();
+        $memberRole->name = 'member';
+        $memberRole->display_name = 'Member';
+        $memberRole->save();
+
+        // Membuat sample admin
+        $admin = new User();
+        $admin->name = 'Admin Larapus';
+        $admin->email = 'admin@larapus.dev';
+        $admin->password = bcrypt('admini');
+        $admin->save();
+        $admin->attachRole($adminRole);
+
+        // Membuat sample member
+        $member = new User();
+        $member->name = 'Sample Member';
+        $member->email = 'member@larapus.dev';
+        $member->password = bcrypt('member');
+        $member->save();
+        $member->attachRole($memberRole);
     }
 }
