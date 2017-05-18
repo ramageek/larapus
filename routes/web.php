aa460fa47e7777522669dff4256c9b48b6cbb49a
ramaqst <qisthi.rm@gmail.com>
2017-05-18 20:43:00 +0700

    buat RESTFUL author di route dan controller

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,12 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+  return view('welcome');
 });
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index');
+Route::group(['prefix'=>'admin','middleware'=>['auth']],function(){
+  Route::resource('authors','AuthorsController');
+});
