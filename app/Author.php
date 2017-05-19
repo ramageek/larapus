ff2f14a6cc14d0eceb5207323e6f1267d9458edd
ramaqst <qisthi.rm@gmail.com>
2017-05-19 19:50:00 +0700

    notif hapus penulis yang memiliki buku

--- app/Author.php
+++ app/Author.php
@@ -3,10 +3,34 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Session;
 
 class Author extends Model
 {
     protected $fillable = ['name'];
+
+    public static function boot(){
+      parent::boot();
+
+      self::deleting(function($author){
+        // Cek buku penulis
+        if ($author->books->count() > 0) {
+          // Pesan error
+          $html = 'Penulis tidak bisa dihapus karena masih memiliki buku : ';
+          $html .= '<ul>';
+          foreach ($author->books as $book) {
+            $html .= '<li>'.$book->title.'</li>';
+          }
+          $html .= '</ul>';
+          Session::flash('flash_notification',[
+            'level'=>'danger',
+            'message'=>$html
+          ]);
+          // Batalkan penghapusan
+          return false;
+        }
+      });
+    }
 
     public function books(){
       return $this->hasMany('App\Book');
--- app/Http/Controllers/AuthorsController.php
+++ app/Http/Controllers/AuthorsController.php
@@ -110,7 +110,7 @@ class AuthorsController extends Controller
      */
     public function destroy($id)
     {
-        Author::destroy($id);
+        if(!Author::destroy($id)) return redirect()->back();
         Session::flash('flash_notification',[
             'level'=>'success',
             'message'=>'Penulis berhasil dihapus'
