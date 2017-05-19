f202b6a4f0b7b3f457827c7d08bfc8807ac62223
ramaqst <qisthi.rm@gmail.com>
2017-05-19 10:26:20 +0700

    role admin di routes & fix routeMiddleware

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index');
-Route::group(['prefix'=>'admin','middleware'=>['auth']],function(){
+Route::group(['prefix'=>'admin','middleware'=>['auth','role:admin']],function(){
   Route::resource('authors','AuthorsController');
 });
