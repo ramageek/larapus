bcbe147d15cd6195b0fabf6124bef44dc984996d
ramaqst <qisthi.rm@gmail.com>
2017-05-23 14:02:53 +0700

    notif member yang belum verif

---
+++ app/Http/Middleware/UserShouldVerified.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+
+class UserShouldVerified
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+        if (Auth::check() && !Auth::user()->is_verified) {
+            Auth::logout();
+
+            Session::flash('flash_notification',[
+                'level'=>'warning',
+                'message'=>'Akun anda belum aktif, silahkan klik link yang telah kami kirim'
+            ]);
+
+            return redirect('/login');
+        }
+        return $response;
+    }
+}
