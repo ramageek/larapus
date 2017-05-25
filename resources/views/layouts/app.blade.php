296ead0d2f25d0ec6391a499b6b7c4d54c1ca1f2
ramaqst <qisthi.rm@gmail.com>
2017-05-26 04:51:36 +0700

    daftar peminjaman admin

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,6 +55,7 @@
                 <li><a href="{{route('authors.index')}}">Penulis</a></li>
                 <li><a href="{{route('books.index')}}">Buku</a></li>
                 <li><a href="{{route('members.index')}}">Member</a></li>
+                <li><a href="{{route('statistics.index')}}">Peminjaman</a></li>
               @endrole
               <li><a href="{{url('/settings/profile')}}">Profil</a></li>
             @endif
--- routes/web.php
+++ routes/web.php
@@ -37,4 +37,8 @@ Route::group(['prefix'=>'admin','middleware'=>['auth','role:admin']],function(){
   Route::resource('authors','AuthorsController');
   Route::resource('books','BooksController');
   Route::resource('members','MembersController');
+  Route::get('statistics',[
+    'as'=>'statistics.index',
+    'uses'=>'StatisticsController@index'
+  ]);
 });
